8ef5f372073121e4194a47c8f3b70cdb0964646f
colonvargas12 <167591046+colonvargas12@users.noreply.github.com>
2026-05-29 11:12:03 -0500

    cambios

--- mi_marca_personal/extra/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.extra_inicio, name='extra_inicio'),
-]
--- mi_marca_personal/extra/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def extra_inicio(request):
-    return HttpResponse("App secundaria creada correctamente. Esta app queda como complemento del proyecto.")
